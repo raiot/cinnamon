dcc28d8a66aee5ebb4bb8fe6a02170cd9c0c928a
Rene Durazo <rdurazo@homeaway.com>
2017-03-06 18:35:27 -0600

    fix issue 3

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -12,25 +12,39 @@ class Nav extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {index: 0};
+        this.handleClick = this.handleClick.bind(this);
+        this.isActive = this.isActive.bind(this);
     }
 
     static defaultProps = {
-        sections: ['Games', 'About Us', 'Contact Us']
+        sections: ['/', 'Games', 'About Us', 'Contact Us']
     };
+
+    handleClick(event) {
+        let index = parseInt(event.target.id.split('menu-')[1], 10);
+        this.setState({index});
+    }
+
+    isActive(index) {
+        return index === this.state.index ? 'active' : '';
+    }
 
     render () {
         let brandImg = '/images/logo.png';
         let gameSections = [];
 
         this.props.sections.forEach((section, index) => {
-            gameSections.push(<li key={index}><Link activeClassName="active" to={`/${Utils.cleanStringAndLowerCase(section)}`}>{section}</Link></li>);
+            if(section !== '/') {
+                gameSections.push(<li key={index} className={this.isActive(index)}><Link id={`menu-${index}`} onClick={this.handleClick} to={`/${Utils.cleanStringAndLowerCase(section)}`}>{section}</Link></li>);
+            }
         });
         return (
             <div className="cinnamon-nav-container">
                 <nav className="navbar navbar-default">
                     <div className="container-fluid">
                         <div className="navbar-header">
-                            <a className="navbar=brand" href="#">
+                            <a id="menu-0" onClick={this.handleClick} className="navbar=brand" href="#">
                                 <img id="cinnamon-brand-logo" alt="Brand" src={brandImg}/>
                             </a>
                             <span className="cinnamon-brand-name">CINNAMON GAMES</span>
